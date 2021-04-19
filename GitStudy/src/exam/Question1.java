a7c7418ffe067ee7879aeddd32524d810c67b4fd
chaHoRyun <ckghfbs@naver.com>
2021-04-19 18:12:50 +0900

    4월 19일 githup & merge 연습끝

--- GitStudy/src/exam/Question1.java
+++ GitStudy/src/exam/Question1.java
@@ -8,20 +8,18 @@ public class Question1 {
 		
 		System.out.println("Have a Good Time");
 		
-<<<<<<< HEAD
 		System.out.println("메인에서 실행함");
-=======
-<<<<<<< HEAD
 		System.out.println("브렌치a 생성기념 실행");
 		System.out.println("브렌치a 생성 기념 2번째 실행");
 		System.out.println("브렌치a 생성 기념 3번째 실행");
 		System.out.println("1 + 5 = 6");
-=======
+
 		System.out.println("브렌치b 생성기념 실행");
 		System.out.println("브렌치b 생성 기념 2번째 실행");
 		System.out.println("브렌치b 생성 기념 3번째 실행");
 		System.out.println("1 + 5 = 15");
->>>>>>> refs/heads/branchB
->>>>>>> refs/heads/branchA
+		
+		System.out.println("연습 끝");
+
 	}
 }
