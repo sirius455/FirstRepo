0008f5d42fd00e58d43e8dbfb509a5b8840758f0
JiWon <jw980806@naver.com>
2021-04-19 11:34:39 +0900

    문제1 풀고 저장

---
+++ GitStudy/src/exam/Question1.java
@@ -0,0 +1,9 @@
+package exam;
+
+public class Question1 {
+	public static void main(String[] args) {
+		System.out.println(" /)/)");
+		System.out.println("( ..)");
+		System.out.println("(  > ¡Ù");
+	}
+}
