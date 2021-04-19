251f0f5605c139710705b24475990aa40c82b9fd
chaHoRyun <ckghfbs@naver.com>
2021-04-19 17:37:40 +0900

    원상복구

--- GitStudy/src/exam/Question1.java
+++ GitStudy/src/exam/Question1.java
@@ -8,20 +8,6 @@ public class Question1 {
 		
 		System.out.println("Have a Good Time");
 		
-<<<<<<< HEAD
-		System.out.println("메인에서 추가");
-=======
-<<<<<<< HEAD
-		System.out.println("branchB 생성 기념1222");
-=======
-		System.out.println("branchA 생성 기념1121233");
->>>>>>> refs/heads/branchA
-		System.out.println("한번더 실행");
-<<<<<<< HEAD
-		System.out.println("또 한번더 실행bb");	
-=======
-		System.out.println("또 한번더 실행");
->>>>>>> refs/heads/branchA
->>>>>>> refs/heads/branchB
+
 	}
 }
