3e416912d3d1fd3c7238a94b945718159b4e2ded
JiWon <jw980806@naver.com>
2021-04-19 11:36:08 +0900

    문제1 수정한 후 저장

--- GitStudy/src/exam/Question1.java
+++ GitStudy/src/exam/Question1.java
@@ -3,7 +3,7 @@ package exam;
 public class Question1 {
 	public static void main(String[] args) {
 		System.out.println(" /)/)");
-		System.out.println("( ..)");
-		System.out.println("(  > ¡Ù");
+		System.out.println("(  xx)");
+		System.out.println("(  > $");
 	}
 }
