2f172379cb94968fdd76a088c61c360b6f994593
horyun <ckghfbs@naver.com>
2021-04-19 15:06:58 +0900

    브렌치 b 수정

--- GitStudy/src/exam/Question2.java
+++ GitStudy/src/exam/Question2.java
@@ -9,7 +9,7 @@ public class Question2 {
 		//branch A,B 모두에 커밋을 2개 추가하고 history 내역을 보낼 것
 		// (단, Question2만 수정해야 함 * 이 주석은 branchB에서 작성되었음)
 		
-		System.out.println("branchB를 실행함");
-		System.out.println("브렌치 bb");
+		System.out.println("1 + 5 = 6");
+	
 	}
 }
