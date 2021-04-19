1ebf1310b29fe0ae5414435d4211af333675e8a4
JiWon <jw980806@naver.com>
2021-04-19 11:46:20 +0900

    2번째 수정

--- GitStudy/src/exam/Question1.java
+++ GitStudy/src/exam/Question1.java
@@ -3,7 +3,7 @@ package exam;
 public class Question1 {
 	public static void main(String[] args) {
 		System.out.println(" /)/)");
-		System.out.println("(  xx)");
+		System.out.println("(  ..)");
 		System.out.println("(  > $");
 	}
 }
