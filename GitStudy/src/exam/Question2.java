da58d06cd7c349a8839e98a9093d74e5fe11b13f
horyun <ckghfbs@naver.com>
2021-04-19 14:49:42 +0900

    브런치 b첫번째 실행

--- GitStudy/src/exam/Question2.java
+++ GitStudy/src/exam/Question2.java
@@ -5,5 +5,10 @@ public class Question2 {
 		System.out.println(Math.random());
 		
 		System.out.println("Random");
+		
+		//branch A,B 모두에 커밋을 2개 추가하고 history 내역을 보낼 것
+		// (단, Question2만 수정해야 함 * 이 주석은 branchB에서 작성되었음)
+		
+		System.out.println("branchB를 실행함");
 	}
 }
